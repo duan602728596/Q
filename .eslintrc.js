d4f24b742c1bb41972ee9b6af9eeb71147294e83
段昊辰 <duanhaochen@126.com>
2021-05-11 17:42:29 +0800

    ci: 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -90,7 +90,7 @@ module.exports = {
       }
     },
     {
-      files: ['*.js', '*.jsx'],
+      files: ['*.js', '*.jsx', '*.mjs', '*.cjs'],
       parser: '@babel/eslint-parser',
       parserOptions: {
         requireConfigFile: false
